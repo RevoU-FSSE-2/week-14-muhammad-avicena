35c31aa067bc5e7deed61f0b80ce1ba254007f3b
Hideaki-Ryu <cenarahmant@gmail.com>
2023-09-27 15:13:04 +0700

    feat: add global types

---
+++ src/types/index.ts
@@ -0,0 +1,22 @@
+export interface Category {
+  id?: number;
+  name: string;
+  is_active: boolean;
+}
+
+export interface DataProfile {
+  name: string;
+  email: string;
+}
+
+export interface FetchListResult<T> {
+  data: T | null;
+  loading: boolean;
+  error: boolean;
+  refresh: () => void;
+}
+
+export interface UserProfileContextType {
+  userProfile: DataProfile | null;
+  setUserProfileData: (data: DataProfile | null) => void;
+}
